51c736ba6171ec9f59d2fb0c1e653f702e8fffcb
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2022-05-20 23:03:26 +0200

    headers añadidos

--- httpd/handlers/estudiantes.go
+++ httpd/handlers/estudiantes.go
@@ -143,5 +143,7 @@ var daniel = model.Alumno{
 var listaAlumnos = []model.Alumno{sebas, rafa, victor, daniel}
 
 func RequestHandler(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Credentials", "true")
 	c.JSON(http.StatusOK, listaAlumnos)
 }
